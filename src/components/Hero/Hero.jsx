115af1a2339687f564cd8776b87bb3e0325ea971
Rashid <rashidiscool21@gmail.com>
2024-06-01 19:01:37 +0100

    Removed insta

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -57,7 +57,7 @@ function Hero({ scrollToAbout, isScrolled }) {
           <a href='https://github.com/Bojackson123' target="_blank" rel="noopener noreferrer"><img className={styles.heroSocials} src={github} alt="Github" /></a>
           <a href='https://www.linkedin.com/in/rashid-al-marri-8b385027a/' target="_blank" rel="noopener noreferrer"><img className={styles.heroSocials} src={linkedin} alt="Linkedin" /></a>
           <a href='https://x.com/RashiidAlmarrii' target="_blank" rel="noopener noreferrer"><img className={styles.heroSocials} src={twitter} alt="Twitter" /></a>
-          <a href='https://www.instagram.com/rashiidalmarrii/' target="_blank" rel="noopener noreferrer"><img className={styles.heroSocials} src={instagram} alt="Instagram" /></a>
+          <a href='' target="_blank" rel="noopener noreferrer"><img className={styles.heroSocials} src={instagram} alt="Instagram" /></a>
           <a href='https://api.whatsapp.com/send/?phone=%2B4407562407672&text&type=phone_number&app_absent=0' target="_blank" rel="noopener noreferrer"><img className={styles.heroSocials} src={whatsapp} alt="Whatsapp" /></a>
         </div>
         <button className={styles.heroButton}>Download CV</button>
