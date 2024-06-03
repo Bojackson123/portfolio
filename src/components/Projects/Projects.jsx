0c81eee85bc52af37f7ca9490ad946fd8182481c
Rashid <rashidiscool21@gmail.com>
2024-06-03 03:57:38 +0100

    Projects scaffolded

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,29 +1,36 @@
 import styles from './Projects.module.css';
-import timeFliesImage from '../../assets/projectPhotos/timeFlies.png';
+import timeFliesImage from '../../assets/projectPhotos/timeFlies.jpg';
 import webapps2024Image from '../../assets/projectPhotos/webapps2024.png';
 import jammmingImage from '../../assets/projectPhotos/jammming.png';
-import ravenousImage from '../../assets/projectPhotos/ravenous.png';
+import ravenousImage from '../../assets/projectPhotos/ravenous.jpg';
+import timeFliesReport from '../../assets/projectPhotos/timeFliesReport.pdf';
+import arrow from '../../assets/icons/arrow-right-top.svg'
 
 const projects = [
     {
         title: "Time Flies",
-        description: "This is a description of Project 1. It includes features and technologies used.",
-        image: timeFliesImage
+        description: "Understanding the methods of how the human mind experiences time passing is at the forefront of psychology and neuroscience research. Dr. Warrick Roseboom and his colleagues at Time Storm have a working AI model, Time Without Clocks, that can accurately predict subjective time from video stimuli. The goal of this goal is to make a web-based tool for researchers to utilize the Time Storm model in a user-friendly and efficient manner. To achieve this, I was tasked with creating a modern front-end web application that can interact with the model, a scalable back-end that stores user’s videos, information, and hosts the AI model, and finally, updating and fine-tuning the AI model to be more modern and efficient.",
+        image: timeFliesImage,
+        link: "https://github.com/Bojackson123/Time-Flies-Frontend",
+        pdfLink: timeFliesReport
     },
     {
         title: "Digital Wallet",
-        description: "This is a description of Project 2. It includes features and technologies used.",
-        image: webapps2024Image
+        description: "This is a description of Project 2.",
+        image: webapps2024Image,
+        link: "https://github.com/Bojackson123/webapps2024"
     },
     {
         title: "Jammming",
-        description: "This is a description of Project 3. It includes features and technologies used.",
-        image: jammmingImage
+        description: "This is a description of Project 3. ",
+        image: jammmingImage,
+        link: "https://jammming-sounds.vercel.app/"
     },
     {
         title: "Ravenous",
-        description: "This is a description of Project 4. It includes features and technologies used.",
-        image: ravenousImage
+        description: "This is a description of Project 4.",
+        image: ravenousImage,
+        link: "https://ravenous-wheat.vercel.app/"
     },
     
 ];
@@ -34,7 +41,7 @@ function Projects() {
 
     return (
         <div className={styles.projectsContainer}>
-            <h1>Projects</h1>
+            <h1>My Projects</h1>
           {projects.map((project, index) => (
             <div key={index} className={`${index % 2 === 0 ? styles.imageLeft : styles.imageRight}`}>
               <img 
@@ -43,8 +50,21 @@ function Projects() {
                 className={styles.projectImage}
               />
             <div className={styles.projectInfo}>
-                <h2>{project.title}</h2>
-                <p>{project.description}</p>
+                <div>
+                    <h2>{project.title}</h2>
+                    <p>
+                        {project.description}&nbsp;
+                        {index === 0 && 
+                        <a href={project.pdfLink} target="_blank" rel="noopener noreferrer">
+                            Read More
+                        </a>}
+                    </p>
+                </div>
+                <div className={`${index % 2 === 0 ? styles.arrowContainerLeft : styles.arrowContainerRight}`}>
+                <a href={project.link} target='_blank'>
+                    <img src={arrow} alt="arrow" className={styles.arrow} />
+                </a>
+                </div>
             </div>
             </div>
           ))}
